f8e61c25791eb84426accbabc5deb4edc036c6ee
Z1V3 <azivko@student.foi.hr>
2023-05-24 20:47:28 -0400

    Added initial design for home page with catalogue for products and added directory for multimedia

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
 $putanja = dirname($_SERVER['REQUEST_URI']);
 
-if($putanja == "/azivko"){
+if ($putanja == "/azivko") {
     $putanja = "/azivko/webdip_projekt";
 }
 require "php/functions.php";
 
+$veza = new Baza();
+$veza->spojiDB();
+
+$upit = "SELECT * FROM intelektualno_vlasnistvo";
+$rezultat = $veza->selectDB($upit);
+
+if ($rezultat->num_rows > 0) {
+    $data = array();
+    while ($row = $rezultat->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,6 +32,15 @@ require "php/functions.php";
             html,body,h1,h2,h3,h4 {
                 font-family:"Lato", sans-serif
             }
+            ul{
+                background-color: darkgray;
+                position: absolute;
+                top: 30%;
+                left: 35%;
+                list-style-type: none;
+                display: grid;
+                grid-template-rows:    repeat(4, 100px);
+            }
             .mySlides {
                 display:none
             }
@@ -32,6 +53,54 @@ require "php/functions.php";
                 padding:0;
                 margin-top:6px
             }
+
+            .panel{
+                position: absolute;
+                top: 10%;
+                left: 37%;
+            }
+
+            .product {
+                display: inline-block;
+                margin: auto;
+                margin-right: 50px;
+                margin-top: 400px;
+                text-align: center;
+                height: 500px;
+                width: 350px;
+                background-color: #494F52;
+            }
+
+            .product img {
+                width: 200px;
+                height: 200px;
+                object-fit: cover;
+            }
+
+            .product h3 {
+                margin-top: 10px;
+            }
+
+            .product p {
+                margin: 10px 0;
+            }
+
+            .product .price {
+                font-weight: bold;
+            }
+
+            h3,p,span {
+                color: white;
+            }
+            h3,span {
+                font-size: 30px;
+            }
+
+            .intellectual_property:hover{
+                color:#000!important;
+                background-color:black !important
+            }
+
         </style>
     </head>
     <body>
@@ -48,47 +117,22 @@ require "php/functions.php";
                 <h1><b>Intelektualna vlasništva</b></h1>
             </div>
 
-            <table class="tablica-popis">
-                <caption>Poruke</caption>
-                <thead>
-                    <tr>
-                        <th>id_poruke</th>
-                        <th>id_status</th>
-                        <th>kategorije</th>
-                        <th>naslov</th>
-                        <th>posiljatelj</th>
-                        <th>primatelj</th>
-                        <th>datum_vrijeme</th>
-                        <th>sadrzaj</th>
-                        <th>prilog</th>
-                    </tr>
-                </thead>
-
-                <tbody> 
-                    <tr>
-                        <td colspan="9">testung</td>
-                    </tr>
-                    <tr>
-                        <td colspan="9">testung</td>
-                    </tr>
-                    <tr>
-                        <td colspan="9">testung</td>
-                    </tr>
-                    <tr>
-                        <td colspan="9">testung</td>
-                    </tr>
-                    <tr>
-                        <td colspan="9">testung</td>
-                    </tr>
-                    <tr>
-                        <td colspan="9">testung</td>
-                    </tr>
-                </tbody>
-
-                <tfoot>
-                </tfoot>
-
-            </table>
+            <?php
+            if (!empty($data)) {
+                foreach ($data as $product) {
+                    echo "<div class='product intellectual_property'>";
+                    echo "<a href='forms/intellectual_property.php?=property_id=$product[intelektualno_vlasnistvo_id]'>";
+                    echo "<img src='multimedia/$product[slika]' alt='$product[slika]' style='height: 200px; width: 200px; background-color: white;'>";
+                    echo "<h3>$product[naziv]</h3>";
+                    echo "<p>$product[opis]</p>";
+                    echo "<span class='price'>$" . $product['cijena_koristenja'] . "</span>";
+                    echo "</a>";
+                    echo "</div>";
+                }
+            } else {
+                echo "No data found.";
+            }
+            ?>
         </div>
     </body>
 </html>
